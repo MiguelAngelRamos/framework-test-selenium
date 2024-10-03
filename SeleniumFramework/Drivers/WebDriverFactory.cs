c1cc576858994e7438c5f6438e6dfe5a8d9a9d2d
Miguel Ramos <return.infox@gmail.com>
2024-10-03 20:53:04 -0300

    iniciando el chrome driver

---
+++ SeleniumFramework/Drivers/WebDriverFactory.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumFramework.Drivers
+{
+    public class WebDriverFactory
+    {
+        public static IWebDriver CreateWebDriver()
+        {
+            // ChromeOptions options = new ChromeOptions();
+            var options = new ChromeOptions();
+            options.AddArguments("--start-maximized");
+            return new ChromeDriver(options);
+        }
+
+
+    }
+}
---
+++ SeleniumFramework/Features/GoogleSearch.feature
@@ -0,0 +1,7 @@
+﻿Feature: Google Search
+  
+  Scenario: Search for a term in Google
+    Given I am on the google search page
+    When I enter "Selenium WebDriver"
+    And I click the search button
+    Then I should see search results
