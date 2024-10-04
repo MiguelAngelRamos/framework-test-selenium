7d721dc3a378f2886f2af05ec74206e139d7f242
Miguel Ramos <return.infox@gmail.com>
2024-10-03 21:00:47 -0300

    framework

---
+++ SeleniumFramework/Pages/GooglePage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumFramework.Pages
+{
+    public class GooglePage
+    {
+        private readonly IWebDriver driver;
+
+        public GooglePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        // Metodo navegar a hacia google
+        public void NavigateToGoogle()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+        }
+    }
+}
