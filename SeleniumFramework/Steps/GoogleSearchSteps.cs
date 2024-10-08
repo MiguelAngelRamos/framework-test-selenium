99212fab9c2c7ad2b4e0f78a44a55b2eac801c33
Miguel Ramos <return.infox@gmail.com>
2024-10-08 19:33:48 -0300

    test google step primera parte

---
+++ SeleniumFramework/Steps/GoogleSearchSteps.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using SeleniumFramework.Pages;
+using TechTalk.SpecFlow;
+
+namespace SeleniumFramework.Steps
+{
+    public class GoogleSearchSteps
+    {
+        private readonly GooglePage googlePage;
+        private readonly IWebDriver driver;
+
+        public GoogleSearchSteps()
+        {
+            googlePage = new GooglePage(driver);
+        }
+
+        [Given(@"I am on the google search page")]
+        public void GivenIAmOnTheGoogleSearchPage()
+        {
+            googlePage.NavigateToGoogle();
+        }
+    }
+}
+
+/*
+ Feature: Característica
+ Scenario: Escenario
+ Given: Dado que / Que
+ When: Cuando
+ And: Y / Además
+ Then: Entonces / Luego
+ */
